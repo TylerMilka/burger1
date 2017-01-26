27fd7c1384f75dd4a5cc52ce798aac403df2df1d
Tyler <tgmilka@gmail.com>
2017-01-25 18:34:33 -0600

    Stil setting up

---
+++ config/orm.js
@@ -0,0 +1,4 @@
+var connection = require('../config/connection.js');
+
+// Inside the `orm.js` file create the code that will execute MySQL commands
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 insert into burgers(burger_name) values ('Cheese Burger');
 insert into burgers(burger_name) values ('Hamburger');
-insert into burgers(burger_name) values ('Bacon Cheese Burger');
-insert into burgers(burger_name) values ('Garlic Burger');
+insert into burgers(burger_name) values ('Fried Egg Cheese Burger');
+insert into burgers(burger_name) values ('Turkey Burger');
