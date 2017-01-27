a3f05b3327a41a67ae7a7d782997b99aff4667bf
Tyler <tgmilka@gmail.com>
2017-01-27 15:27:28 -0600

    Updating views

---
+++ view/views.js
@@ -0,0 +1,10 @@
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
+
+module.exports = connection;
